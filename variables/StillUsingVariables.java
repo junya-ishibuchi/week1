ea630004b3ff94c0f8d9014a8a443126f2dd5136
Junya Ishibuchi <junya.ishibuchi@gmail.com>
2021-10-08 17:01:51 -0700

    Add StillUsingVariables

---
+++ variables/StillUsingVariables.java
@@ -0,0 +1,10 @@
+package week1.variables;
+
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        String name = "Junya Ishibuchi";
+        int graduateYear = 2023;
+
+        System.out.printf("My name is %s and I'll graduate in %d.%n", name, graduateYear);
+    }
+}
